06fa10490a9939ee374b5f78f2687a8f89e6709f
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2023-07-13 15:20:54 +0530

    Update StringLength.c

--- String/StringLength.c
+++ String/StringLength.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main()
+{
+    char str[]="WelcomeToIndia";
+    int i;
+    for(i=0;str[i] != '\0';i++)
+    {
+       
+    }
+    printf("Length Of String=%d",i);
+    return 0;
+}
