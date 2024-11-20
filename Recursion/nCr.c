de0dfb97954381536d216df591ccf047ec72eea9
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2024-11-20 13:00:26 +0530

    nCr.c

---
+++ Recursion/nCr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fact(int n)
+{
+    if(n<=1)
+    return n;
+    else
+    return fact(n-1)*n;
+}
+int ncr(int n, int r)
+{
+    int t0,t1,t2;
+    t0=fact(n);
+    t1=fact(r);
+    t2=fact(n-r);
+    
+    return (t0/t1 *(t2));
+}
+int main()
+{
+    printf("%d",ncr(5,4));
+}
