fdbed5b42ca7d9e54109677cabcb6f8e2c1e5657
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2024-11-23 23:10:12 +0530

    BinarySearch.c

---
+++ Array/BinarySearch.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct array{
+    int data[10];
+    int size;
+    int len;
+};
+void display(struct array arr)
+{
+ int i;
+ for(i=0;i<arr.len-1;i++)
+ {
+     printf("%d ",arr.data[i]);
+ }
+}
+int Binary_search(struct array arr,int key)
+{
+    int l=0;
+    int mid;
+    int h=arr.len-1;
+    while(l <= h)
+    {
+        mid=(l+h)/2;
+        
+        if(key==arr.data[mid])
+        return mid;
+        else if(key > arr.data[mid])
+           l=mid+1;
+        else
+           h=mid-1;
+    }
+    return 0;
+}
+int main()
+{
+    struct array arr={{2,3,4,9,6,13,23,75,82,94},11,11};
+    display(arr);
+    int x=Binary_search(arr,4);
+    if(x)
+    {
+        printf("found\n");
+    }
+    else
+    {
+       printf("Not found\n"); 
+    }
+    return 0;
+}
