ebd0c23a5595484881631050c8957fac516edb95
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2024-11-28 17:21:15 +0530

    Pair_Of_Kth_Sum.c

--- Array/PairOf_Kth_Sum.c
+++ Array/PairOf_Kth_Sum.c
@@ -8,11 +8,10 @@ int main()
     int i,j;
     int k=10;
     int n=sizeof(arr)/sizeof(arr[0]);
-    for(i=0;i<n;i++)
+    for(i=0;i<=n-1;i++)
     {
-     for(j=0;j<n;j++)
+     for(j=i+1;j<=n;j++)
      {
-         if(i !=j)
          {
          if(arr[i]+arr[j]==k)
          printf("%d %d\n",arr[i],arr[j]);
