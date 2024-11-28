63a064e5d8ecd02c1bdd7e5969fdab4a97d32b50
rohitpatil597 <30457945+rohitpatil597@users.noreply.github.com>
2024-11-28 19:33:50 +0530

    length_of_string.c

---
+++ String/length_of_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int len,i;
+    char str[]="welcome";
+    for(i=0;str[i] != '\0';i++)
+    {
+
+    }
+    printf("%d",i);
+    return 0;
+}
